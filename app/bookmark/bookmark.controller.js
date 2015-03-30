0148a02265206f984995ae08201e56bd202f3c58
Tom Joseph <tom.joseph@detroitlabs.com>
2015-03-30 16:04:28 -0400

    stubs for module, controller

--- app/bookmark/bookmark.controller.js
+++ app/bookmark/bookmark.controller.js
@@ -1,1 +1,20 @@
-ng
+(function() {
+    'use strict';
+
+    angular
+        .module('bookmark')
+        .controller('BookmarkController', Controller);
+
+    /* @ngInject */
+    function Controller(dependencies) {
+        var vm = this;
+        vm.title = 'BookmarkController';
+
+        activate();
+
+        ////////////////
+
+        function activate() {
+        }
+    }
+})();
