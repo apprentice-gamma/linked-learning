22340465bfecc9f5010decd24303b99fa4d39cb8
Tom Joseph <tom.joseph@detroitlabs.com>
2015-03-30 16:16:21 -0400

    working angular seed

--- app/bookmark/bookmark.controller.js
+++ app/bookmark/bookmark.controller.js
@@ -1,20 +1,4 @@
-(function() {
-    'use strict';
-
-    angular
-        .module('bookmark')
-        .controller('BookmarkController', Controller);
-
-    /* @ngInject */
-    function Controller(dependencies) {
-        var vm = this;
-        vm.title = 'BookmarkController';
-
-        activate();
-
-        ////////////////
-
-        function activate() {
-        }
-    }
-})();
+angular.module('app').controller("MainController", function() {
+    var vm = this;
+    vm.title = "Bookmark Controller";
+});
