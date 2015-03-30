cfb449107fe9b0a239b279066647cd76f25bcb73
Tom Joseph <tom.joseph@detroitlabs.com>
2015-03-30 16:28:55 -0400

    added bookmark object, display bookmark object

--- app/bookmark/bookmark.controller.js
+++ app/bookmark/bookmark.controller.js
@@ -10,11 +10,18 @@
     function BookmarkController() {
         var vm = this;
         vm.title = "Bookmark Controller Outside";
+        vm.bookmark = {};
 
-        activate();
+        activate(vm);
 
         ////////////////
 
-        function activate() {}
+        function activate(vm) {
+        	vm.bookmark.url = "http://www.google.com/";
+        	vm.bookmark.date = Date.now();
+        	vm.bookmark.user = "Robot";
+        	vm.bookmark.comment = "A search engine";
+        	vm.bookmark.title = "Google!";
+        }
     }
 })();
