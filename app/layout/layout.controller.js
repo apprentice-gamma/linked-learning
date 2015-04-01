b07ce88585c31c41725f0e4c26e0db903952651c
Tom Joseph <tom.joseph@detroitlabs.com>
2015-04-01 16:34:48 -0400

    able to swap between RH column templates

--- app/layout/layout.controller.js
+++ app/layout/layout.controller.js
@@ -8,12 +8,23 @@
         vm.title = 'Layout Controller';
         vm.pageL = 'listings';
         vm.pageR = 'add';
+        vm.swapRightCol = swapRightCol;
 
         activate();
 
         ////////////////
 
         function activate() {
+        }
+
+        function swapRightCol() {
+        	if (vm.pageR === 'add') {
+        		vm.pageR ='comments';
+        	} else if (vm.pageR === 'comments') {
+        		vm.pageR = 'add';
+        	} else {
+        		console.log('ERROR IN RH COL SWAP');
+        	}
         }
     }
 })();
