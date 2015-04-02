75487139c7f9cb894840176807f93dabf9e12b4e
Tom Joseph <tom.joseph@detroitlabs.com>
2015-04-02 13:29:58 -0400

    adding route parameter, access the route parameter in detail page

--- app/details-page/detail.ctrl.js
+++ app/details-page/detail.ctrl.js
@@ -3,8 +3,9 @@ angular
 	.module('linked-learning')
 	.controller('DetailController', DetailController);
 
-	function DetailController (){
+	function DetailController ($routeParams){
 		var vm = this;
+		vm.urlParameter = $routeParams.bookmarkURL;
 		vm.bookmark = {title: 'W3Schools', url: "http://www.w3schools.com/"};
 
 	}
