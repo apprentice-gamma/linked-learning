f55628754c69f882b6ed7ad24471cecbd5e844bb
Tom Joseph <tom.joseph@detroitlabs.com>
2015-04-06 14:53:26 -0400

    using cookies example code from a tutorial. store and retrieve username into a cookie. use  to send the user object to the cookie controller

--- app/user/user.controller.js
+++ app/user/user.controller.js
@@ -1,30 +1,41 @@
 angular
 	.module('linked-learning')
-	.controller('AuthCtrl', ['$scope', 'GooglePlus', function($scope, GooglePlus) {
-			$scope.user = {};
-			
-            $scope.login = function() {
-                GooglePlus.login().then(function(authResult) {
-                    console.log(authResult);
+	.controller('AuthCtrl', ['$scope', 'GooglePlus', GoogleCTRL]);
 
-                    GooglePlus.getUser().then(function(user) {
-                        console.log(user);
-                        $scope.user = user;
-                    });
-                }, function(err) {
-                    console.log(err);
+	function GoogleCTRL($scope, GooglePlus) {
+		$scope.user = {};		
+        $scope.login = function() {
+            GooglePlus.login().then(function(authResult) {
+                console.log(authResult);
+
+                GooglePlus.getUser().then(function(user) {
+                    console.log(user);
+                    $scope.user = user;
+                    $scope.$broadcast('LOGGED_IN_WITH_GOOGLE', user);
                 });
-            };
-        }]);
+            }, function(err) {
+                console.log(err);
+            });
+        };
+    }
 
 angular
 	.module('linked-learning')
 	.controller('CookieControl', ['$scope', '$cookies','$cookieStore', CookieCTRL]);
 
 	function CookieCTRL($scope, $cookies, $cookieStore) {
-		$cookies.userName = 'Sandeep';
-  		$scope.platformCookie = $cookies.userName;
-  		$cookieStore.put('fruit','Apple');
-  		$cookieStore.put('flower','Rose');
-  		$scope.myFruit= $cookieStore.get('fruit');
+		$scope.myFruit = 'Banana';
+		$scope.name = 'NOT LOGGED IN';
+
+		$scope.$on('LOGGED_IN_WITH_GOOGLE', function(event, data) {
+			$scope.user = data;
+
+			$cookies.userName = $scope.user.name;
+	  		$scope.platformCookie = $cookies.userName;
+	  		$cookieStore.put('fruit','Apple');
+	  		$cookieStore.put('flower','Rose');
+			$cookieStore.put('name', $scope.user.name);
+	  		$scope.myFruit= $cookieStore.get('fruit');
+	  		$scope.name = $cookieStore.get('name');
+		});
 	}
