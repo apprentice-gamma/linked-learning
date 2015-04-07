0b108a2cdc7d84ef691cc3da14cbd0961faa7ee4
Linda Swanson <linda.swanson@detroitlabs.com>
2015-04-07 15:48:38 -0400

    Removed X on modal. Don't allow user to click off the modal without logging in.

--- app/user/user.directives.js
+++ app/user/user.directives.js
@@ -16,6 +16,6 @@ angular.module('linked-learning').directive('modalDialog', function() {
         scope.show = false;
       };
     },
-    template: "<div class='ng-modal' ng-show='show'><div class='ng-modal-overlay' ng-click='hideModal()'></div><div class='ng-modal-dialog' ng-style='dialogStyle'><div class='ng-modal-close' ng-click='hideModal()'>X</div><div class='ng-modal-dialog-content' ng-transclude></div></div></div>"
+    templateUrl: "../../templates/partial-modal.html"
   };
 });
