a641742f9459b967308395a53b4d193abb603832
Ivan <ivan.herndon@detroitlabs.com>
2015-04-09 13:47:31 -0400

    refactor layout controller

--- app/listings/listings.controller.js
+++ app/listings/listings.controller.js
@@ -57,7 +57,7 @@
             }
 
             BookmarkFactory.curIndex = index;
-            layoutController.pageR ='comments';
+            layoutController.pageRight ='comments';
         }
     }
 })();
