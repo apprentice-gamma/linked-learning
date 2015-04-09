fe93b863e784e95216988ce9a93850df92a05025
Ivan <ivan.herndon@detroitlabs.com>
2015-04-09 11:30:25 -0400

    moved bookmark factory into seperate file. added script to index to reference bookmark factory file

--- app/linked-learning.app.js
+++ app/linked-learning.app.js
@@ -1,23 +1,6 @@
 angular.module('linked-learning', ['ngRoute',  'ngCookies', 'ui.bootstrap', 'googleplus']);
 angular.module('linked-learning').config(Configuration);
-angular.module('linked-learning').factory('BookmarkFactory', BookmarkFactory);
 
-function BookmarkFactory($http) {
-    var url = 'https://project1-backend.herokuapp.com/api/bookmarks';
-    var factory = {};
-    factory.getBookmarks = function(){
-        return $http.get(url);
-    };
-    factory.addBookmark = function(bookmark){
-        return $http.post(url, bookmark);
-    };
-    factory.addComment = function(bookmark, comment){
-        return $http.post(url + '/' + bookmark._id+ '/' + 'comments', comment);
-    };
-    factory.curIndex = 0;
-
-    return factory;
-}
 
 function Configuration($routeProvider, GooglePlusProvider) {
     var home = '/';
